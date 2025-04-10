26b545b6387a75d05eb224ce779d7f873d6c57ad
Shorabh Singh <157907232+shorabh9@users.noreply.github.com>
2025-04-11 01:21:35 +0530

    Fix: corrected userSlice import path

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import {Link, NavLink} from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { authenticateUser, logoutUser } from '../reduxSlices/userSlice';
+import { authenticateUser, logoutUser } from '../reduxSlices/UserSlice';
 import { addToCart, deleteItem } from '../reduxSlices/cartSlice';
 import Menu from './Menu';
 import B1 from "../assets/B12.jpg";
