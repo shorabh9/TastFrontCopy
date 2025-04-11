cc78ee768b731fc7a7603b3df7aec0876115fa2f
Shorabh Singh <157907232+shorabh9@users.noreply.github.com>
2025-04-11 15:17:59 +0530

    Update Layout.jsx

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -4,6 +4,7 @@ import Footer from './pages/Footer'
 import { Outlet } from 'react-router-dom'
 
 function Layout() {
+   const hideHeaderRoutes = ['/profile'];
   return (
     <>
      {!hideHeaderRoutes.includes(location.pathname) && <Header />}
